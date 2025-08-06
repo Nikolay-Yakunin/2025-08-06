753f760762ce47e727d099e4daec6eac5a76db13
Nikolay-Yakunin <akuninn52@gmail.com>
2025-08-07 00:54:44 +0700

    add Archiver

---
+++ internal/archiver/archiver.go
@@ -0,0 +1,75 @@
+package archiver
+
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// Нужно нормальное название.
+type Archiver interface {
+	CreateZip(files []string, dest string) error
+}
+
+type ZipArchiver struct{} // any не подходит.
+
+// Конструктор архиватора.
+func NewZipArchiver() *ZipArchiver {
+	return &ZipArchiver{}
+}
+
+// Создает и заполняет zip архив.
+func (a *ZipArchiver) CreateZip(files []string, dest string) error {
+	// Проверка существования директории.
+	dir := filepath.Dir(dest)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	zipFile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer zipFile.Close()
+
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
+
+	// Добавление файлов в архив.
+	for _, file := range files {
+		if err := addFileToZip(zipWriter, file); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Добавляет файл в архив.
+func addFileToZip(zipWriter *zip.Writer, filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	info, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		return err
+	}
+	header.Name = filepath.Base(filename)
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(writer, file)
+	return err
+}
